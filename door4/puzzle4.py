5094c278eb7afe669736aecba2ae7f5d3d7c80da
Max Stubenbord <maxos@Maxs-MacBook-Pro.local>
2020-12-07 14:13:48 +0100

    finished pz4

--- door4/puzzle4.py
+++ door4/puzzle4.py
@@ -1,6 +1,7 @@
+# --- Day 4: Passport Processing ---
+
 import re
 from requests import get
-import pprint
 
 
 def get_puzzle_input() -> str:
@@ -20,17 +21,47 @@ def get_arr_of_dict() -> str:
     return pzz_input
 
 
-def check_valid():
+def get_count_and_valid_entries():
     count = 0
+    valid_entries = []
     for entry in get_arr_of_dict():
         if entry.get('cid'):
             entry.pop('cid')
         if len(entry) == 7:
+            valid_entries.append(entry)
             count += 1
+    return count, valid_entries
+
+
+print(get_count_and_valid_entries()[0])
+
+# --- Part Two ---
+
+validation_params = {
+    'byr': '(19[2-9]|200[0-2])',
+    'iyr': '(201[0-9]|2020)',
+    'eyr': '(202[0-9]|2030)',
+    'hgt': '^(1[5-8][0-9]|19[0-3])\s?cm$|^(59|6[0-9]|7[0-6])\s?in$',
+    'hcl': '^#(\d|[a-f]){6}$',
+    'ecl': '^(amb|blu|brn|gry|grn|hzl|oth)$',
+    'pid': '^[0-9]{9}$',
+    'cid': '(.*?)'
+}
+
+
+def check_with_valid_params():
+    count = 0
+    _, valid_entries = get_count_and_valid_entries()
+    for entry in valid_entries:
+        count += 1 if is_matching(entry) else 0
     return count
 
 
-print(check_valid())
+def is_matching(entry):
+    for key, value in entry.items():
+        if not bool(re.match(validation_params[key], value)):
+            return False
+    return True
 
-validation_params = {
-    'byr': }
+
+print(check_with_valid_params())
